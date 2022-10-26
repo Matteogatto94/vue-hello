fd88e5fc878d08b4697bff37bdab7ba6812d83e6
Matteogatto94 <cosenza94@live.it>
2022-10-26 15:53:26 +0200

    add h1 with data and finish exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
 /*Descrizione:
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.*/
+
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Sono Matteo, studente di Classe 72'
+      }
+    }
+  }).mount('#app')
